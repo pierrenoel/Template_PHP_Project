2f8df01a15277b8e58cda0f22dd4aceeeab45ac5
pierrenoel <pierrenoel1509@gmail.com>
2023-01-21 10:49:42 +0100

    update

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends Controller
     */
     public function index()
     {
-        return $this->view('welcome',["name" => "Cogip"]);
+        return $this->view('welcome',["name" => "you"]);
     }
 }
--- Resources/views/welcome.php
+++ Resources/views/welcome.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/css/reset.css" rel="stylesheet" type="text/css">
     <link href="assets/css/styles.css" rel="stylesheet" type="text/css">
-    <title>Cogip - Welcome</title>
+    <title>Template PHP Project - Welcome</title>
 </head>
 <body>
     <main>
@@ -16,7 +16,7 @@
                 alt="cogip logo"
                 class="illustration">
             <h1>Welcome to <?php echo $name ?></h1>
-            <p>This base project is provided by BeCode</p>
+            <p>This base project is provided by Pierre</p>
             <ul>
                 <li><a href="https://github.com/bramus/router" target="_blank">Bramus/Router</a></li>
                 <li><a href="https://github.com/filp/whoops" target="_blank">Flip/Whoops</a></li>
